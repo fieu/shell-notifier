6f5d4819da6299d2405f5bd03df45d3ef154d5a1
Sheldon Rupp <me@suce.dev>
2019-08-06 16:30:22 +0200

    Fix typo

--- notify.sh
+++ notify.sh
@@ -13,7 +13,7 @@ curl -s "http://ip-api.com/json/${USER_IP}" > $TMPFILE
 # Execute webhook
 discord.sh \
     --webhook-url "$WEBHOOK_URL" \
-    --username "ShellBot"
-    --avatar "https://i.imgur.com/iXIShwr.png"
+    --username "ShellBot" \
+    --avatar "https://i.imgur.com/iXIShwr.png" \
     --title "Shell Opened" \
     --description "New \`$(basename "$SHELL")\` shell opened.\n\n**Details**\n:small_blue_diamond: Host: \`$(hostname -f)\`\n:small_blue_diamond: User: \`$(whoami)\`\n:small_blue_diamond: TTY: \`${SSH_TTY}\`\n\n**IP Address Information**\n:small_orange_diamond: IP: \`${USER_IP}\`\n:small_orange_diamond: Country: \`$(cat $TMPFILE | jq -r .country)\`\n:small_orange_diamond: City: \`$(cat $TMPFILE | jq -r .city)\`\n:small_orange_diamond: Region: \`$(cat $TMPFILE | jq -r .regionName)\`\n:small_orange_diamond: ISP: \`$(cat $TMPFILE | jq -r .isp)\`"
