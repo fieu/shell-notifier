1645350794cbb3e75b38e89a194f06430a74798c
Sheldon Rupp <me@suce.dev>
2019-08-06 15:01:28 +0200

    Add README and add basename to notify.sh

---
+++ README.md
@@ -0,0 +1,32 @@
+<h1 align="center">
+  <br>
+  shell-notifier
+  <br>
+</h1>
+
+<h4 align="center">Send a notification via Discord on every new SSH login</h4>
+
+## Proposal
+I wanted a notification for every time a new SSH session has begun.
+
+## Requirements
+* [cURL](https://curl.haxx.se/)
+* [jq](https://stedolan.github.io/jq/)
+* [discord.sh](https://github.com/ChaoticWeg/discord.sh) (assumes you have it installed in your `$PATH`)
+
+## Install
+Simply add the following lines to your `~/.bashrc`, `~/.zshrc` or other sourced file.
+```sh
+# Discord webhook (put the webhook URL inside the quotes)
+WEBHOOK_URL=""
+# Execute webhook
+curl -sL https://raw.githubusercontent.com/NurdTurd/shell-notifier/master/notify.sh | bash -s "$WEBHOOK_URL"
+```
+
+## Usage
+Simply open a shell!
+
+### Example
+Discord webhook example:
+
+![Screenshot](https://i.imgur.com/Dpp3qNW.png)
--- notify.sh
+++ notify.sh
@@ -8,10 +8,10 @@ TMPFILE=$(mktemp)
 USER_IP=$(echo $SSH_CLIENT | awk '{ print $1}')
 
 # Grab IP information 1 time and cache it
-curl -s "https://ipinfo.io/${USER_IP}/json" > $TMPFILE
+curl -s "http://ip-api.com/json/${USER_IP}" > $TMPFILE
 
 # Execute webhook
 discord.sh \
     --webhook-url "$WEBHOOK_URL" \
     --title "Shell Opened" \
-    --description "New \`${SHELL}\` shell opened.\n\n**Details**\n:small_blue_diamond: Host: \`$(hostname -f)\`\n:small_blue_diamond: User: \`$(whoami)\`\n:small_blue_diamond: TTY: \`${SSH_TTY}\`\n\n**IP Address Information**\n:small_orange_diamond: IP: \`${USER_IP}\`\n:small_orange_diamond: Hostname: \`$(cat $TMPFILE | jq -r .hostname)\`\n:small_orange_diamond: Country: \`$(cat $TMPFILE | jq -r .country)\`\n:small_orange_diamond: City: \`$(cat $TMPFILE | jq -r .city)\`\n:small_orange_diamond: Region: \`$(cat $TMPFILE | jq -r .region)\`"
+    --description "New \`$(basename "$SHELL")\` shell opened.\n\n**Details**\n:small_blue_diamond: Host: \`$(hostname -f)\`\n:small_blue_diamond: User: \`$(whoami)\`\n:small_blue_diamond: TTY: \`${SSH_TTY}\`\n\n**IP Address Information**\n:small_orange_diamond: IP: \`${USER_IP}\`\n:small_orange_diamond: Country: \`$(cat $TMPFILE | jq -r .country)\`\n:small_orange_diamond: City: \`$(cat $TMPFILE | jq -r .city)\`\n:small_orange_diamond: Region: \`$(cat $TMPFILE | jq -r .regionName)\`\n:small_orange_diamond: ISP: \`$(cat $TMPFILE | jq -r .isp)\`"
